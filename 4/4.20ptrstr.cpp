d4ea073361e4486b1fc987cabd01d0273fcaaca9
Simon Liu <silver_eagle@sina.com>
2016-04-06 14:45:04 +0800

    字符串指针

---
+++ 4/4.20ptrstr.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+int main(int argc, char const *argv[])
+{
+	using namespace std;
+	char animal[20] = "bear";
+	const char * bird = "wren";
+	char * ps;
+
+	cout << animal << " and ";
+	cout << bird <<"\n";
+	//cout << ps << "\n";
+
+	cout << "Enter a kind animal: ";
+	cin >> animal;
+
+	ps = animal;
+	cout << ps << "!\n";
+	cout << "Before uysing strcpy() : \n";
+	cout << animal << " at " << (int *) animal << endl;
+	cout << ps << " at " << (int *) ps << endl;
+
+	ps = new char[strlen(animal) + 1];
+	strcpy(ps, animal);
+	cout << "After using strcpy() : \n";
+	cout << animal << " at " << (int *) animal << endl;
+	cout << ps << " at " << (int *) ps << endl;
+	delete [] ps;
+
+	return 0;
+}
